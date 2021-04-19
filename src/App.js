bdb48996b99fa4b5bcf3741a2a32cf68d2f39101
Lynda Sowunmi <lyndasowunmi@yahoo.com>
2021-04-19 13:04:17 -0500

    Select JSON data needed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
+
+import  { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+import { fecthData } from './api';
+
 class App extends React.Component {
+
+ async componentDidMount() {
+    const data = await fecthData();
+
+     console.log(data);
+ }
+
  render() {
     return (
-       <div>
-           <h1>App</h1>
+       <div className= {styles.container}>
+           <Cards />
+           <CountryPicker />
+           <Chart />
        </div>
     )
   }
